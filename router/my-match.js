e4c72d4aaf183590014794131409514ca1eae205
jungyu <jungyu4281@gmail.com>
2025-01-01 17:49:05 +0900

    캘린더, 매니저 역할 부여 일부 완료
    캘린더, 매니저 역할 부여 일부 완료

---
+++ router/my-match.js
@@ -0,0 +1,6 @@
+// import express from 'express';
+// import { fetchMyMatches } from '../controller/my-matches.js';
+
+
+// const router = express.Router();
+// router.post('/my-matches', fetchMyMatches);
--- router/schedule-list.js
+++ router/schedule-list.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { fetchMatchSchedule } from '../controller/schedule-list.js';
+import { fetchMyMatches } from '../controller/my-matches.js';
 import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/schedule-list', isAuth, fetchMatchSchedule);
+router.post('/my-matches', fetchMyMatches);
 
 export default router;
